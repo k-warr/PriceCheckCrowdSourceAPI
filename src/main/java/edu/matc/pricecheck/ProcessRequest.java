d2a9bd83848518882f73f458513e6b84d4be93d6
Eric <monforte@madisoncollege.edu>
2017-03-15 22:06:17 -0500

    still working on process request

--- src/main/java/edu/matc/pricecheck/ProcessRequest.java
+++ src/main/java/edu/matc/pricecheck/ProcessRequest.java
@@ -2,7 +2,10 @@ package edu.matc.pricecheck;
 
 import com.fasterxml.jackson.core.JsonProcessingException;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import edu.matc.persistence.StoreDao;
 import org.apache.log4j.Logger;
+
+import java.util.List;
 
 /**
  * Created by student on 3/4/17.
@@ -19,7 +22,7 @@ public class ProcessRequest {
     private final Logger log = Logger.getLogger(this.getClass());
 
     public ProcessRequest() {
-
+        response = new Request();
     }
 
     public ProcessRequest(Request request) {
@@ -45,5 +48,30 @@ public class ProcessRequest {
 
     private void processEntry() {
 
+        for (EntryItem entry: request.getEntry()) {
+            processItems(entry);
+            procesGrocery(request.getUserLatitude(), request.getUserLongtitude(), (double) request.getRadiusMile());
+        }
+
+    }
+
+    private void procesGrocery(double userLatitude, double userLongtitude, double radiusMile) {
+        List<Integer> grocerId = new StoreDao().getNearestStore(userLatitude,
+                userLongtitude, radiusMile);
+    }
+
+
+    private void processItems(EntryItem entryItem) {
+        for (ItemsItem item: entryItem.getItems()) {
+            processItem(item);
+        }
+    }
+
+    private void processItem(ItemsItem item) {
+        ItemsItem newItem = item;
+        item.getName();
+        item.getBrand();
+        item.getUnit();
+        item.getUnitValue();
     }
 }
