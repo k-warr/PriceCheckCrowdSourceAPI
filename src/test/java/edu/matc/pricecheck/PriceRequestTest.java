5feeb0c881186fc4029c15a108d97bfc15b8ca47
Eric <monforte@madisoncollege.edu>
2017-03-28 20:11:04 -0500

    baseline

--- src/test/java/edu/matc/pricecheck/PriceRequestTest.java
+++ src/test/java/edu/matc/pricecheck/PriceRequestTest.java
@@ -1,63 +1,63 @@
-
-package edu.matc.pricecheck;
-
-import com.sun.deploy.config.ClientConfig;
-import org.junit.Before;
-import org.junit.Test;
-
-import javax.ws.rs.client.Client;
-import javax.ws.rs.client.ClientBuilder;
-import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
-
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static edu.matc.pricecheck.PriceRequest.getNearestGroceryStores;
-import static org.junit.Assert.*;
-
-
-/**
- * Created by student on 3/23/17.
- */
-
-public class PriceRequestTest {
-
-    WebTarget target;
-    String pathVars;
-
-    @Before
-    public void setup() {
-        ClientConfig config = new ClientConfig();
-        Client client = ClientBuilder.newClient();
-        target = client.target("http://localhost:8080/pricerequest");
-        pathVars = "";
-
-    }
-
-    @Test
-    public void getHelloTest() {
-        System.out.println(target.request().accept(MediaType.TEXT_PLAIN).get(String.class));
-    }
-
-    @Test
-    public void getMsgPlainJSONTest() {
-        System.out.println(target.request().accept(MediaType.TEXT_PLAIN).get(String.class));
-//        String output = target.path("param=param&key=value/HTML").request().accept(MediaType.TEXT_HTML).get(String.class);
-//        String expected = "<html> <title>Hello Jersey</title><body><h1>Hello param=param&key=value in HTML</h1></body></html>";
-//        System.out.println(output);
-//        assertEquals(expected, output);
-    }
-
-    @Test
-    public void getNearestGroceryStoresTest() throws Exception {
-        Map<String, Map<String, String>> results = new HashMap<String, Map<String, String>>();
-
-        results = getNearestGroceryStores("43.105825", "-89.336998", "1");
-
-        System.out.println(results);
-    }
-
-
-}
+//
+//package edu.matc.pricecheck;
+//
+//import com.sun.deploy.config.ClientConfig;
+//import org.junit.Before;
+//import org.junit.Test;
+//
+//import javax.ws.rs.client.Client;
+//import javax.ws.rs.client.ClientBuilder;
+//import javax.ws.rs.client.WebTarget;
+//import javax.ws.rs.core.MediaType;
+//
+//
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//import static edu.matc.pricecheck.PriceRequest.getNearestGroceryStores;
+//import static org.junit.Assert.*;
+//
+//
+///**
+// * Created by student on 3/23/17.
+// */
+//
+//public class PriceRequestTest {
+//
+//    WebTarget target;
+//    String pathVars;
+//
+//    @Before
+//    public void setup() {
+//        ClientConfig config = new ClientConfig();
+//        Client client = ClientBuilder.newClient();
+//        target = client.target("http://localhost:8080/pricerequest");
+//        pathVars = "";
+//
+//    }
+//
+//    @Test
+//    public void getHelloTest() {
+//        System.out.println(target.request().accept(MediaType.TEXT_PLAIN).get(String.class));
+//    }
+//
+//    @Test
+//    public void getMsgPlainJSONTest() {
+//        System.out.println(target.request().accept(MediaType.TEXT_PLAIN).get(String.class));
+////        String output = target.path("param=param&key=value/HTML").request().accept(MediaType.TEXT_HTML).get(String.class);
+////        String expected = "<html> <title>Hello Jersey</title><body><h1>Hello param=param&key=value in HTML</h1></body></html>";
+////        System.out.println(output);
+////        assertEquals(expected, output);
+//    }
+//
+//    @Test
+//    public void getNearestGroceryStoresTest() throws Exception {
+//        Map<String, Map<String, String>> results = new HashMap<String, Map<String, String>>();
+//
+//        results = getNearestGroceryStores("43.105825", "-89.336998", "1");
+//
+//        System.out.println(results);
+//    }
+//
+//
+//}
