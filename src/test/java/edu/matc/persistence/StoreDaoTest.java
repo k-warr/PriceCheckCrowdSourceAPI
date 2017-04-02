8dbcc03b213e4373cdb48bf50fc4cd53a1999f8f
Eric <monforte@madisoncollege.edu>
2017-04-01 22:28:03 -0500

    added new user and started documenting and creating test

--- src/test/java/edu/matc/persistence/StoreDaoTest.java
+++ src/test/java/edu/matc/persistence/StoreDaoTest.java
@@ -1,22 +1,114 @@
-//package edu.matc.persistence;
-//
-//import edu.matc.entity.Store;
-//import org.junit.Test;
-//
-//import java.util.List;
-//
-//import static org.junit.Assert.assertEquals;
-//
-///**
-// * Created by student on 3/19/17.
-// */
-//public class StoreDaoTest {
-//    @Test
-//    public void getNearestStore() throws Exception {
-//        StoreDao dao = new StoreDao();
-//        List<Store> stores = dao.getNearestStore( 43.121842,
-//                -89.328031, 5.0);
-//        assertEquals("Not matching", 5, stores.size());
-//    }
-//
-//}
+package edu.matc.persistence;
+
+import edu.matc.entity.Store;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.math.BigDecimal;
+import java.util.List;
+
+import static junit.framework.TestCase.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
+
+/**
+ * Created by student on 4/1/17.
+ */
+public class StoreDaoTest {
+    Store store;
+    Store store2;
+    StoreDao dao;
+    int storeId;
+
+    @Before
+    public void setUp() throws Exception {
+        store = new Store();
+        dao = new StoreDao();
+
+        store.setStoreName("JUnit Store");
+        store.setStoreAddress("JUnit Address");
+        store.setLongtitude(BigDecimal.valueOf(5.0));
+        store.setLatitude(BigDecimal.valueOf(5.0));
+
+        storeId = dao.addStore(store);
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+        dao.deleteStore(store);
+
+    }
+
+    @Test
+    public void addStore() throws Exception {
+
+        assertTrue("Store ID > 0",(storeId > 0));
+
+        store2 = dao.getStore(storeId);
+        assertTrue("Stores should be equal",(store2.getStoreId() == store.getStoreId()));
+
+    }
+
+    @Test
+    public void deleteStore() throws Exception {
+        store2 = new Store();
+        store2.setStoreName("JUnit Store2");
+        store2.setStoreAddress("JUnit Address2");
+        store2.setLongtitude(BigDecimal.valueOf(65.0));
+        store2.setLatitude(BigDecimal.valueOf(65.0));
+
+        int storeId2 = dao.addStore(store2);
+        dao.deleteStore(store2);
+        Store storeTemporary = dao.getStore(storeId2);
+
+        assertNull("Should be null",storeTemporary);
+
+    }
+
+    @Test
+    public void getAllStores() throws Exception {
+        List<Store> stores = dao.getAllStores();
+        boolean found = false;
+        assertTrue("Nothing came back",(stores.size() > 0));
+
+        for (Store store3: stores) {
+            if (store3.getStoreId() == store.getStoreId()) {
+                found = true;
+            }
+        }
+
+        assertTrue("Store Should be found ",found);
+    }
+
+    @Test
+    public void getStoreByName() throws Exception {
+
+        assertEquals("Store id should match",(Integer) storeId, (Integer) dao
+                .getStoreByName("JUnit Store").get(0));
+
+    }
+
+    @Test
+    public void getExactStore() throws Exception {
+        assertEquals("Store should match", store.getStoreId(), dao.getExactStore(store
+                .getStoreName(), store.getLatitude().doubleValue(), store
+                .getLongtitude().doubleValue()).get(0).getStoreId());
+    }
+
+    @Test
+    public void getNearestStoreId() throws Exception {
+        List<Integer> storeIds = dao.getNearestStoreId(5,5,50);
+        assertEquals("Shnould equal",storeId, (int) storeIds.get(0));
+
+    }
+
+    @Test
+    public void getNearestStore() throws Exception {
+        List<Store> stores = dao.getNearestStore(5,5,50);
+        assertEquals("Shnould equal",store.getStoreId(), stores.get(0).getStoreId());
+
+    }
+
+}
--- src/test/java/edu/matc/pricecheck/PriceRequestTest.java
+++ src/test/java/edu/matc/pricecheck/PriceRequestTest.java
@@ -8,14 +8,9 @@ import org.junit.Test;
 import javax.ws.rs.client.Client;
 import javax.ws.rs.client.ClientBuilder;
 import javax.ws.rs.client.WebTarget;
-import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
-
-import java.util.HashMap;
-import java.util.Map;
-
-import static edu.matc.pricecheck.PriceRequest.getNearestGroceryStores;
-import static org.junit.Assert.*;
+import static org.junit.Assert.assertEquals;
 
 
 /**
@@ -23,9 +18,8 @@ import static org.junit.Assert.*;
  */
 
 public class PriceRequestTest {
-
-    WebTarget target;
-    String pathVars;
+    PriceRequest priceRequest;
+    Response response;
 
     @Before
     public void setup() {
@@ -33,8 +27,53 @@ public class PriceRequestTest {
         Client client = ClientBuilder.newClient();
         target = client.target("http://localhost:8080/pricerequest");
         pathVars = "";
-
+        priceRequest = new PriceRequest();
     }
+    @Test
+    public void addItemJSON() throws Exception {
+        response = priceRequest.addItemJSON("<testItem>",99.99,
+                "<testItemUnit>", "99", "<testBrandName",
+               "<testStoreName>", "<testStoreAddress>", 0.000000, 0.0000,
+                "system");
+        assertEquals("Return code should be 500", 500, response
+                .getStatus());
+        assertEquals("Message expected is not met", "{\"message\" : \"Added Successfully!\"}" , response.getEntity().toString());
+    }
+
+    @Test
+    public void addItemHTML() throws Exception {
+        response = priceRequest.addItemHTML("<testItem>",99.99,
+                "<testItemUnit>", "99", "<testBrandName",
+                "<testStoreName>", "<testStoreAddress>", 0.000000, 0.0000,
+                "system");
+        assertEquals("Return code should be 500", 500, response
+                .getStatus());
+        assertEquals("Message expected is not met", "<h2><span>message:</span>Added Successfully!</h2>" , response.getEntity().toString());
+    }
+
+    @Test
+    public void addNewUserJSON() throws Exception {
+        response = priceRequest.addNewUserJSON();
+        assertEquals("Return code should be 300", 300, response
+                .getStatus());
+        assertEquals("Message expected is not met", "{\"apiKey\" : ",
+                response.getEntity().toString().substring(0,12));
+    }
+
+    @Test
+    public void addNewUserHTML() throws Exception {
+        response = priceRequest.addNewUserHTML();
+        assertEquals("Return code should be 300", 300, response
+                .getStatus());
+        assertEquals("Message expected is not met",
+                "<h2><span>apiKey:</span>",
+                response.getEntity().toString().substring(0,24));
+    }
+
+    WebTarget target;
+    String pathVars;
+
+
 
 //    @Test
 //    public void getHelloTest() {
